1db05e17d4b0ff012d475aaa374bb7e82db1bc4e
Mohid <mohid10587@gmail.com>
2022-09-15 20:58:55 +0600

    71.3 / 2.10

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
-    <div className="App">
-     <div className="alert alert-info shadow-lg">
-  <div>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-    <span>New software update available.</span>
-  </div>
-</div>
+    
+    <div>
+      <Navbar></Navbar>
+<Routes>
+  <Route path='/' element={<Home></Home>}></Route>
+  <Route path='about' element={<About></About>}></Route>
 
+</Routes>
 
-
-<h1 className="text-3xl text-red-500"> this is heading</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,22 @@ module.exports = {
    content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+
+    daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
   },
+
  plugins: [require("daisyui")],
 }
