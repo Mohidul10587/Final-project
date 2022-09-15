04153d6d337c348776ffed0fe95f36759a3174e5
mohid10587@gmail.com <mohid10587@gmail.com>
2022-09-16 00:28:55 +0600

    nice massage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            This is home page 
+            This is home page and a nice page 
         </div>
     );
 }
