677cb3a7742ec97b474400895e1e16b301fc4a60
mohid10587@gmail.com <mohid10587@gmail.com>
2022-09-16 20:16:28 +0600

    add icons

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import InfoCard from './InfoCard.js'
 import clock from '../../assets/icons/clock.svg'
+import marker from '../../assets/icons/marker.svg'
+import  from '../../assets/icons/.svg'
 const Info = () => {
     return (
         <div>
           <InfoCard img={clock} />
-          <InfoCard img={clock} />
+          <InfoCard img={marker} />
           <InfoCard img={clock} />
         </div>
     );
