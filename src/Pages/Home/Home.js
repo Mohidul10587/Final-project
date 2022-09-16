780fa0538052d2b1f76f0f8051da19df8b88dff7
mohid10587@gmail.com <mohid10587@gmail.com>
2022-09-16 23:10:50 +0600

    Added Card

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from'./Banner'
 import Info from './Info'
 import Services from './Services'
+import Card from './Card'
 
 const Home = () => {
     return (  
@@ -9,6 +10,7 @@ const Home = () => {
        <Banner />
        <Info />
        <Services />
+       <Card/>
     </>
     );
 }
