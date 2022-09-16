cce4e4cdddd7bb54ebcf258d8a598c15d4c54f04
mohid10587@gmail.com <mohid10587@gmail.com>
2022-09-16 20:34:26 +0600

    add title

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -2,13 +2,13 @@ import React from 'react';
 import InfoCard from './InfoCard.js'
 import clock from '../../assets/icons/clock.svg'
 import marker from '../../assets/icons/marker.svg'
-import  from '../../assets/icons/.svg'
+import phone from '../../assets/icons/phone.svg'
 const Info = () => {
     return (
         <div>
-          <InfoCard img={clock} />
-          <InfoCard img={marker} />
-          <InfoCard img={clock} />
+          <InfoCard cardTitle='Opening Hours' img={clock} />
+          <InfoCard cardTitle='xxxxxxxxxxxxx' img={marker} />
+          <InfoCard cardTitle='yyyyyyyyuyyuy' img={phone} />
         </div>
     );
 }
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 
-const InfoCard = ({img}) => {
+const InfoCard = ({img, cardTitle}) => {
     return (
      <div className="card lg:card-side bg-accent shadow-xl">
   <figure><img src={img} alt="Album"/></figure>
   <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
+    <h2 className="card-title">{cardTitle}</h2>
     <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      
-    </div>
+    
   </div>
 </div>
     );
