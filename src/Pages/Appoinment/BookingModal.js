afa19e1f5366308c6c8b24e70b45d8a7fe45ddcb
mohid10587@gmail.com <mohid10587@gmail.com>
2022-09-18 14:26:13 +0600

    6.6 running

--- src/Pages/Appoinment/BookingModal.js
+++ src/Pages/Appoinment/BookingModal.js
@@ -11,7 +11,8 @@ const BookingModal = ({ treatment }) => {
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
 
                     <h3 className="font-bold text-lg">{name}</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <form>
+                    </form>
                     <div className="modal-action">
                         <label htmlFor="booking-modal" className="btn">Yay!</label>
                     </div>
