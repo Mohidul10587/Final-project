26212e97db758f10a8f8a473212bdfd85f8c8e03
mohid10587@gmail.com <mohid10587@gmail.com>
2022-09-18 19:28:44 +0600

    url added

--- doctors-server /index.js
+++ doctors-server /index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 
@@ -10,6 +12,15 @@ app.use(express.json())
 
 
 
+
+const uri = "mongodb+srv://doctors_admin:Tch2hZziDCHZmT8@cluster0.jrhw6sp.mongodb.net/?retryWrites=true&w=majority";
+
+/* const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});*/
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
