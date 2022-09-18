cff7d2b034deffaf89a80d32a524187ad19a1e97
mohid10587@gmail.com <mohid10587@gmail.com>
2022-09-18 22:11:46 +0600

    database integration

--- doctors-server /index.js
+++ doctors-server /index.js
@@ -12,15 +12,24 @@ app.use(express.json())
 
 
 
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jrhw6sp.mongodb.net/?retryWrites=true&w=majority";
 
-const uri = "mongodb+srv://doctors_admin:Tch2hZziDCHZmT8@cluster0.jrhw6sp.mongodb.net/?retryWrites=true&w=majority";
-
-/* const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+/*
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
 });*/
+
+async function run() {
+try {
+  await client.connect()
+} finally {
+  
+}
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
