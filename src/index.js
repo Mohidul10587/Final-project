3a868ed55e2d736148f38acc795f838234b87a43
mohid <mohid10587@gmail.com>
2022-10-16 09:18:41 +0600

    74.3 is completed

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import {
-  useQuery,
-  useMutation,
-  useQueryClient,
   QueryClient,
   QueryClientProvider,
 } from 'react-query'
